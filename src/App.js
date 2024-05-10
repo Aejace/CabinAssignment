3dba5acf96ccc95c1dc326af0e004503644d527f
Arlo Jones <67977487+Aejace@users.noreply.github.com>
2024-05-09 23:12:34 -0700

    Updating to containers

--- src/App.js
+++ src/App.js
@@ -1,51 +1,161 @@
-import React, {useState} from 'react';
 import './App.css';
 import logo from './logo.svg';
 
-import {DndContext} from '@dnd-kit/core';
+import React, { useState } from "react";
+import {
+  DndContext,
+  DragOverlay,
+  closestCorners,
+  KeyboardSensor,
+  PointerSensor,
+  useSensor,
+  useSensors
+} from "@dnd-kit/core";
+import { arrayMove, sortableKeyboardCoordinates } from "@dnd-kit/sortable";
 
-import {Droppable} from './Droppable';
-import {Draggable} from './Draggable';
+import Container from "./container";
+import { Item } from "./sortable_item";
 
-function App() {
-  const containers = ['A', 'B', 'C'];
-  const [parent, setParent] = useState(null);
-  const draggableMarkup = (
-    <Draggable id="draggable">Drag me</Draggable>
+const wrapperStyle = {
+  flexDirection: "row"
+};
+
+export default function App() {
+  const [items, setItems] = useState({
+    root: ["1", "2", "3"],
+    container1: ["4", "5", "6"],
+    container2: ["7", "8", "9"],
+    container3: []
+  });
+  const [activeId, setActiveId] = useState();
+
+  const sensors = useSensors(
+    useSensor(PointerSensor),
+    useSensor(KeyboardSensor, {
+      coordinateGetter: sortableKeyboardCoordinates
+    })
   );
 
   return (
-    <div className="App">
+    <div className="App" style={wrapperStyle}>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Cabin Assignments
         </p>
       </header>
-      <body>
-        <DndContext onDragEnd={handleDragEnd}>
-          {parent === null ? draggableMarkup : null}
-
-          {containers.map((id) => (
-            // We updated the Droppable component so it would accept an `id`
-            // prop and pass it to `useDroppable`
-            <Droppable key={id} id={id}>
-              {parent === id ? draggableMarkup : 'Drop here'}
-            </Droppable>
-          ))}
+      <DndContext
+        announcements={defaultAnnouncements}
+        sensors={sensors}
+        collisionDetection={closestCorners}
+        onDragStart={handleDragStart}
+        onDragOver={handleDragOver}
+        onDragEnd={handleDragEnd}
+      >
+        <Container id="root" items={items.root} />
+        <Container id="container1" items={items.container1} />
+        <Container id="container2" items={items.container2} />
+        <Container id="container3" items={items.container3} />
+        <DragOverlay>{activeId ? <Item id={activeId} /> : null}</DragOverlay>
       </DndContext>
-      </body>
     </div>
-    
   );
 
-  function handleDragEnd(event) {
-    const {over} = event;
+  function findContainer(id) {
+    if (id in items) {
+      return id;
+    }
 
-    // If the item is dropped over a container, set it as the parent
-    // otherwise reset the parent to `null`
-    setParent(over ? over.id : null);
+    return Object.keys(items).find((key) => items[key].includes(id));
   }
-};
 
-export default App;
+  function handleDragStart(event) {
+    const { active } = event;
+    const { id } = active;
+
+    setActiveId(id);
+  }
+
+  function handleDragOver(event) {
+    const { active, over, draggingRect } = event;
+    const { id } = active;
+    const { id: overId } = over;
+
+    // Find the containers
+    const activeContainer = findContainer(id);
+    const overContainer = findContainer(overId);
+
+    if (
+      !activeContainer ||
+      !overContainer ||
+      activeContainer === overContainer
+    ) {
+      return;
+    }
+
+    setItems((prev) => {
+      const activeItems = prev[activeContainer];
+      const overItems = prev[overContainer];
+
+      // Find the indexes for the items
+      const activeIndex = activeItems.indexOf(id);
+      const overIndex = overItems.indexOf(overId);
+
+      let newIndex;
+      if (overId in prev) {
+        // We're at the root droppable of a container
+        newIndex = overItems.length + 1;
+      } else {
+        const isBelowLastItem =
+          over &&
+          overIndex === overItems.length - 1 &&
+          draggingRect.offsetTop > over.rect.offsetTop + over.rect.height;
+
+        const modifier = isBelowLastItem ? 1 : 0;
+
+        newIndex = overIndex >= 0 ? overIndex + modifier : overItems.length + 1;
+      }
+
+      return {
+        ...prev,
+        [activeContainer]: [
+          ...prev[activeContainer].filter((item) => item !== active.id)
+        ],
+        [overContainer]: [
+          ...prev[overContainer].slice(0, newIndex),
+          items[activeContainer][activeIndex],
+          ...prev[overContainer].slice(newIndex, prev[overContainer].length)
+        ]
+      };
+    });
+  }
+
+  function handleDragEnd(event) {
+    const { active, over } = event;
+    const { id } = active;
+    const { id: overId } = over;
+
+    const activeContainer = findContainer(id);
+    const overContainer = findContainer(overId);
+
+    if (
+      !activeContainer ||
+      !overContainer ||
+      activeContainer !== overContainer
+    ) {
+      return;
+    }
+
+    const activeIndex = items[activeContainer].indexOf(active.id);
+    const overIndex = items[overContainer].indexOf(overId);
+
+    if (activeIndex !== overIndex) {
+      setItems((items) => ({
+        ...items,
+        [overContainer]: arrayMove(items[overContainer], activeIndex, overIndex)
+      }));
+    }
+
+    setActiveId(null);
+  }
+}
